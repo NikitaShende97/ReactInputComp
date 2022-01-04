f9dda203f59b2f0616e81c1bc8115026a88bfde1
NikitaShende97 <nikitashendenn@gmail.com>
2022-01-04 14:13:25 +0530

    b4

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import { useState } from "react";
 
 export default function App() {
-  let [counter, setCounter] = useState(0);
-  let list = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
-  const increment = () => {
-    counter = counter + 1;
-    setCounter(counter);
-  };
   return (
     <div>
-      <h1>Counter Application</h1>
-      <input type="button" value="Click" onClick={increment}></input>
-      {list.map(() => (
-        <div>like {counter}</div>
-      ))}
+      <MyComponent />
+    </div>
+  );
+}
+
+function MyComponent() {
+  const [msg, setMsg] = useState("");
+
+  const changeMessage = (e) => setMsg(e.target.value);
+  return (
+    <div>
+      <div>
+        <input type="text" onChange={changeMessage}></input>
+      </div>
+      <div>{msg}</div>
+      <div>{msg}</div>
+      <div>{msg}</div>
+      <div>{msg}</div>
     </div>
   );
 }
