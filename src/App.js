0469b86c88c074c40f65da287d0209693f5260f1
NikitaShende97 <nikitashendenn@gmail.com>
2022-01-04 21:42:10 +0530

    b11

--- src/App.js
+++ src/App.js
@@ -3,93 +3,27 @@ import { useState } from "react";
 export default function App() {
   return (
     <div>
-      <MyComponent />
+      <Mycomponenet />
     </div>
   );
 }
 
-function MyComponent() {
-  const [Username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [email, setEmail] = useState("");
-  const [mobile, setMobile] = useState("");
-  const [UserList, setUserList] = useState([]);
-  const [validateUser, setValidation] = useState(false);
-
-  const handleUsername = (e) => {
-    setUsername(e.target.value);
-  };
-  const handlePassword = (e) => {
-    setPassword(e.target.value);
-  };
-  const handleEmail = (e) => {
-    setEmail(e.target.value);
-  };
-  const handleMobile = (e) => {
-    setMobile(e.target.value);
-  };
-
-  const register = () => {
-    const UserObj = {
-      username: Username,
-      password: password,
-      email: email,
-      mobile: mobile,
-    };
-
-    if (Username == "" || password == "" || email == "" || mobile == "") {
-      setValidation(true);
-      return;
-    }
-
-    const newUserList = [UserObj, ...UserList];
-    setUserList(newUserList);
-    setValidation(false);
-    setUsername("");
-    setPassword("");
-    setEmail("");
-    setMobile("");
+function Mycomponenet() {
+  const [AjaxList, setAjaxList] = useState([]);
+  const MakeAjaxCall = async () => {
+    const url = "https://jsonplaceholder.typicode.com/posts";
+    const response = await fetch(url);
+    const result = await response.json();
+    setAjaxList(result);
   };
   return (
     <div>
       <div>
-        <input
-          type="text"
-          placeholder="Enter User name"
-          value={Username}
-          onChange={handleUsername}
-          className={
-            Username == "" && validateUser ? "border border-danger" : ""
-          }
-        ></input>
-        <input
-          type="text"
-          placeholder="Enter password"
-          value={password}
-          onChange={handlePassword}
-          className={
-            password == "" && validateUser ? "border border-danger" : ""
-          }
-        ></input>
-        <input
-          type="text"
-          placeholder="Enter email"
-          value={email}
-          onChange={handleEmail}
-          className={email == "" && validateUser ? "border border-danger" : ""}
-        ></input>
-        <input
-          type="text"
-          placeholder="Enter mobile"
-          value={mobile}
-          onChange={handleMobile}
-          className={mobile == "" && validateUser ? "border border-danger" : ""}
-        ></input>
-        <input type="button" value="Register" onClick={register}></input>
+        <input type="button" value="Ajax Call" onClick={MakeAjaxCall}></input>
         <div>
-          {UserList.map((item, index) => (
-            <div>
-              {item.username},{item.password},{item.email},{item.mobile}
+          {AjaxList.map((item, index) => (
+            <div className="bg-warning mb-1" key={index}>
+              {item.title}
             </div>
           ))}
         </div>
