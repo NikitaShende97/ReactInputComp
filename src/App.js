567f1f210eabdada1d4dfed23b24474d625f9671
NikitaShende97 <nikitashendenn@gmail.com>
2022-01-04 13:19:07 +0530

    b3

--- src/App.js
+++ src/App.js
@@ -1,45 +1,18 @@
 import { useState } from "react";
 
 export default function App() {
-  let list = [1, 1, 1, 1, 1, 1];
-
-  const [cardCss, setCss] = useState("bg-primary text-white m-1 p-2");
-
-  const primaryTheme = () => setCss("bg-primary text-white m-1 p-2");
-
-  const infoTheme = () => setCss("bg-info text-white m-1 p-2");
-
-  const DangerTheme = () => setCss("bg-danger text-white m-1 p-2");
-
+  let [counter, setCounter] = useState(0);
+  let list = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+  const increment = () => {
+    counter = counter + 1;
+    setCounter(counter);
+  };
   return (
     <div>
-      <input
-        type="button"
-        value="Primary Theme"
-        onClick={primaryTheme}
-        className="btn btn-primary"
-      ></input>
-
-      <input
-        type="button"
-        value="Info Theme"
-        onClick={infoTheme}
-        className="btn btn-info"
-      ></input>
-
-      <input
-        type="button"
-        value="Danger Theme"
-        onClick={DangerTheme}
-        className="btn btn-danger"
-      ></input>
+      <h1>Counter Application</h1>
+      <input type="button" value="Click" onClick={increment}></input>
       {list.map(() => (
-        <div className={cardCss}>
-          lorem CDAC_Programs\WPTWeb\ReactJs\project4>git remote ad d origin
-          https://github.com/NikitaShende97/ReactInputComp.gitCDAC_Programs\W
-          PTWeb\ReactJs\project4>git remote add origin
-          https://github.com/NikitaShende97/ReactInputComp.git
-        </div>
+        <div>like {counter}</div>
       ))}
     </div>
   );
