cf02e7469f7c82ddb648de3abc45ec5f9f183cb7
NikitaShende97 <nikitashendenn@gmail.com>
2022-01-04 13:05:49 +0530

    b2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-export default function App() {
-  return (
-    <div>
-      <MyComponent />
-    </div>
-  );
-}
+import { useState } from "react";
 
-function MyComponent() {
-  let id = 101;
-  let str = "Nikita";
-  let list = ["nikita", "vinayak"];
-  let obj = { id: 1, uname: "Nevase" };
+export default function App() {
+  let list = [1, 1, 1, 1, 1, 1];
+
+  const [cardCss, setCss] = useState("bg-primary text-white m-1 p-2");
+
+  const primaryTheme = () => setCss("bg-primary text-white m-1 p-2");
+
+  const infoTheme = () => setCss("bg-info text-white m-1 p-2");
+
+  const DangerTheme = () => setCss("bg-danger text-white m-1 p-2");
+
   return (
     <div>
-      <h1>Hello {str}</h1>
-      <h3>your id is {id}</h3>
-      <h5>
-        {list[0].toUpperCase()},{list[1].toUpperCase()}
-      </h5>
-      <h6>
-        {obj.id},{obj.uname}
-      </h6>
+      <input
+        type="button"
+        value="Primary Theme"
+        onClick={primaryTheme}
+        className="btn btn-primary"
+      ></input>
+
+      <input
+        type="button"
+        value="Info Theme"
+        onClick={infoTheme}
+        className="btn btn-info"
+      ></input>
+
+      <input
+        type="button"
+        value="Danger Theme"
+        onClick={DangerTheme}
+        className="btn btn-danger"
+      ></input>
+      {list.map(() => (
+        <div className={cardCss}>
+          lorem CDAC_Programs\WPTWeb\ReactJs\project4>git remote ad d origin
+          https://github.com/NikitaShende97/ReactInputComp.gitCDAC_Programs\W
+          PTWeb\ReactJs\project4>git remote add origin
+          https://github.com/NikitaShende97/ReactInputComp.git
+        </div>
+      ))}
     </div>
   );
 }
