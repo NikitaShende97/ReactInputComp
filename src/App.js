a18fe4dfcadfc3aeae1571bf48d6c89e10040e4b
NikitaShende97 <nikitashendenn@gmail.com>
2022-01-04 17:11:41 +0530

    b8

--- src/App.js
+++ src/App.js
@@ -3,35 +3,47 @@ import { useState } from "react";
 export default function App() {
   return (
     <div>
-      <Mycomponent />
+      <Mycomponenet />
     </div>
   );
 }
+function Mycomponenet() {
+  const [list, setList] = useState(["Hello"]);
+  const [Msg, setMsg] = useState("");
 
-function Mycomponent() {
-  const [list, setList] = useState([""]);
-  const [msg, setMsg] = useState("");
-
-  const changeMsg = (e) => {
-    setMsg(e.target.value);
-  };
-  const tweet = () => {
-    const newList = [msg, ...list];
+  const tweet = (e) => {
+    const newList = [Msg, ...list];
     setList(newList);
     setMsg("");
+  };
+  const changeMsg = (e) => {
+    const newMsg = e.target.value;
+    setMsg(newMsg);
   };
   const deleteTop = () => {
     list.splice(0, 1);
     setList([...list]);
   };
+  const deleteIndex = (index1) => {
+    list.splice(index1, 1);
+    setList([...list]);
+  };
   return (
     <div>
       <input type="text" onChange={changeMsg}></input>
-      <input type="button" value="Tweet" onClick={tweet}></input>
-      <input type="button" value="delete Top" onClick={deleteTop}></input>
+      <input type="button" onClick={tweet} value="Tweet"></input>
+      <input type="button" onClick={deleteTop} value="DeleteTop"></input>
       <div>
-        {list.map((item) => (
-          <div>{item}</div>
+        {list.map((item, index1) => (
+          <div>
+            {item}
+
+            <input
+              type="button"
+              onClick={() => deleteIndex(index1)}
+              value="Delete"
+            ></input>
+          </div>
         ))}
       </div>
     </div>
