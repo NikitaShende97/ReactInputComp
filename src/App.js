bd23ddac1d6df98a3cf3034ed4eb0b3e8411f31f
NikitaShende97 <nikitashendenn@gmail.com>
2022-01-04 12:45:52 +0530

    b1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MyComponent />
     </div>
   );
 }
 
-export default App;
+function MyComponent() {
+  let id = 101;
+  let str = "Nikita";
+  let list = ["nikita", "vinayak"];
+  let obj = { id: 1, uname: "Nevase" };
+  return (
+    <div>
+      <h1>Hello {str}</h1>
+      <h3>your id is {id}</h3>
+      <h5>
+        {list[0].toUpperCase()},{list[1].toUpperCase()}
+      </h5>
+      <h6>
+        {obj.id},{obj.uname}
+      </h6>
+    </div>
+  );
+}
