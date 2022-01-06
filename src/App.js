92cc847272df4b8dc1d137217ac3697ede5bc7f6
NikitaShende97 <nikitashendenn@gmail.com>
2022-01-06 13:05:49 +0530

    b11

--- src/App.js
+++ src/App.js
@@ -3,30 +3,64 @@ import { useState } from "react";
 export default function App() {
   return (
     <div>
-      <Mycomponenet />
+      <Mycomponent />
     </div>
   );
 }
 
-function Mycomponenet() {
-  const [AjaxList, setAjaxList] = useState([]);
-  const MakeAjaxCall = async () => {
-    const url = "https://jsonplaceholder.typicode.com/posts";
-    const response = await fetch(url);
-    const result = await response.json();
-    setAjaxList(result);
+function Mycomponent() {
+  const [movie, setMovie] = useState("");
+  const [year, setYear] = useState("");
+  const [list, setList] = useState(["Movie"]);
+
+  const handleMoviename = (e) => {
+    setMovie(e.target.value);
+  };
+  const handleYear = (e) => {
+    setYear(e.target.value);
+  };
+  const addDetails = () => {
+    const userObj = {
+      Movie: movie,
+      Year: year,
+    };
+    const newList = [...list, userObj];
+    setList(newList);
+  };
+
+  const deleteMovie = (index) => {
+    list.splice(index, 1);
+    setList([...list]);
   };
   return (
     <div>
       <div>
-        <input type="button" value="Ajax Call" onClick={MakeAjaxCall}></input>
-        <div>
-          {AjaxList.map((item, index) => (
-            <div className="bg-warning mb-1" key={index}>
-              {item.title}
+        <input type="text" onChange={handleMoviename}></input>
+        <input type="text" onChange={handleYear}></input>
+        <input
+          type="button"
+          onClick={addDetails}
+          value="Add"
+          className="btn btn-outline-primary btn-sm"
+        ></input>
+      </div>
+      <div>
+        {list.map((item, index) => (
+          <div key={index}>
+            <div className="row bg-secondary mt-1 text-white">
+              <div className="col-1">{item.Movie}</div>
+              <div className="col-1">{item.Year}</div>
+              <div className="col-3">
+                <input
+                  type="button"
+                  value="delete"
+                  onClick={() => deleteMovie(index)}
+                  className="btn btn-primary"
+                ></input>
+              </div>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
     </div>
   );
